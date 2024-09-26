af4c7d22936fa2d6c39c891aa561a94f31ec237a
mohammadbarakat950 <40238983@hruc.ac.uk>
2024-09-26 10:44:06 +0100

    Update PythonApplication3.py

--- PythonApplication3.py
+++ PythonApplication3.py
@@ -1,17 +1,9 @@
-def quicksort(arr, left, right):
-    if left < right:
-        partition_opp = partition(arr, left, right) 
-        quicksort(arr, left, partition_pos - 1) 
-        quicksort(arr, left, parition_pos + 1, right)
-        
-
-def parition(arr, left, right):
-    i = left
-    j = right -1 
-    pivot = arr(right)
-    
-    while i < j:
-        while i < right and arr[i] < pivot 
-                i += 1 
-        while j > left and arr[j] >= 
-        
+def insertion_sort(arr):
+    for i in range(1, len(arr)):
+        j = i
+        while arr[j - 1] > arr[j] and j > 0:
+            arr[j - 1], arr[j] = arr[j], arr[j -1]
+            j -= 1
+arr = [2,6,5,1,3,4]
+insertion_sort(arr)
+print(arr)
